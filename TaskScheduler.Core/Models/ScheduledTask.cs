87bac04ff48e4c597edfce5dfa7aa4197828b1da
Pakhapong Kanchanamai <Pakhapong.Kanchanamai@nikon.com>
2026-01-06 16:51:03 +0700

    OK

--- TaskScheduler.Core/Models/ScheduledTask.cs
+++ TaskScheduler.Core/Models/ScheduledTask.cs
@@ -7,11 +7,7 @@ namespace TaskScheduler.Core.Models
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
 
-        // ลบ ApiUrl, HttpMethod, Headers, Body เดิมออก
-
-        // เพิ่มความสัมพันธ์แบบ One-to-Many กับ TaskStep
         public ICollection<TaskStep> Steps { get; set; } = new List<TaskStep>();
-
         public ICollection<TaskTrigger> Triggers { get; set; } = new List<TaskTrigger>();
     }
 }
